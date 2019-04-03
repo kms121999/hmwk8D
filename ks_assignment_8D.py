f86b6ba2fe4ff766820e9f7583c6a656502088a6
Keaton <ksmith36@gmail.com>
2019-04-02 20:25:40 -0500

    Built some messy logic

--- ks_assignment_8D.py
+++ ks_assignment_8D.py
@@ -4,7 +4,12 @@
 #
 #
 
-RECORDS = 'student_files8D/Records.
+
+'''
+
+'''
+
+RECORDS = "student_files8D/Records.xml" #??? is this right?
 
 def main():
     #
@@ -31,3 +36,65 @@ def getSongs():
     #
 
     pass
+
+
+#scratch
+
+xml = '''<Discography>
+	<Album Artist="Mark Ronson Featuring Bruno Mars">
+		<Track>
+			<Name>
+				Uptown Funk!
+			</Name>
+			<Length>
+				4:30
+			</Length>
+		</Track>
+	</Album>
+	<Album Artist="Mark Ronson Featuring Bruno Mars">
+		<Track>
+			<Name>
+				Thinking Out Loud
+			</Name>
+			<Length>
+				4:41
+			</Length>
+		</Track>
+	</Album>
+
+
+
+
+
+
+</Discography>'''
+
+
+songs = []
+count = 0
+name = False
+length = False
+
+for line in xml.split('\n'):
+    if '<Album' in line:
+        songs.append({})
+        b = line.split('"')
+        songs[count]['artist'] = b[1]
+
+    if name:
+        name = False
+        songs[count]['name'] = line.strip()
+
+    if length:
+        length = False
+        songs[count]['length'] = line.strip()
+
+    if '<Name' in line:
+        name = True
+
+    if '<Length' in line:
+        length = True
+
+    if '/Album' in line:
+        count += 1
+        
