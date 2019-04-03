c3e5fb6c7f6bc7dea998f4069d51d111e8b45029
Keaton <ksmith36@gmail.com>
2019-04-02 19:57:42 -0500

    Began developing program skeleton.

---
+++ ks_assignment_8D.py
@@ -0,0 +1,33 @@
+#
+#
+#
+#
+#
+
+RECORDS = 'student_files8D/Records.
+
+def main():
+    #
+    #
+    #
+    #
+
+    pass
+
+
+def loadFile():
+    #
+    #
+    #
+    #
+
+    pass
+
+def getSongs():
+    #
+    #
+    #
+    #
+    #
+
+    pass
